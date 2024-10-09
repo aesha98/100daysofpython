1076b18bb78e3cdd52dc13504fe38eed53b05283
aesha98 <sityasya@gmail.com>
2024-10-10 02:58:55 +0800

    update winning and lose condition

--- Day_6_7/main.py
+++ Day_6_7/main.py
@@ -3,16 +3,20 @@
 
 import random
 import hangman_words, hangman_art
+import string
 
-# TEST: print random words
 game = 1
 guess_word = random.choice(hangman_words.list_words)
 word_to_guess = guess_word.split()
 word = []
 word_to_fill = []
+lives = 6
 
 # display logo
 print(hangman_art.logo)
+
+for char in guess_word:
+	word.append(char)
 
 # game running
 while(game):
@@ -20,15 +24,18 @@ while(game):
 	print("word to guess: " + guess_word)
 	user_input_letter = input("Guess a letter: ").lower()
 
-	# push char into new list
-	for char in guess_word:
-		word.append(char)
-	
 	# check answer
 	for letter in word:
-		if letter == user_input_letter:		
+		if user_input_letter == letter:		
 			word_to_fill.append(user_input_letter)
 			print(word_to_fill)
 			break
-
-
+		elif lives == 0:
+			game = 0
+			break
+		else:
+			print(f"you guess {user_input_letter}. That's not in the word. You lose a life.")
+			print(hangman_art.stages[lives - 1])
+			print(f"************ You have {lives - 1}/6 lives left ************************")
+			lives -= 1
+			break
