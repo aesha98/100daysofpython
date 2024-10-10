23d10151676059732ff37089cb3ad6bb0dd8aef2
aesha98 <sityasya@gmail.com>
2024-10-10 12:54:06 +0800

    update hangman game

--- Day_6_7/main.py
+++ Day_6_7/main.py
@@ -11,31 +11,40 @@ word_to_guess = guess_word.split()
 word = []
 word_to_fill = []
 lives = 6
+placeholder = ""
 
 # display logo
 print(hangman_art.logo)
 
-for char in guess_word:
+for char in len(guess_word):
+	placeholder += '_'
 	word.append(char)
 
 # game running
 while(game):
-
-	print("word to guess: " + guess_word)
+	print("word to guess: " + placeholder)
+	print(f"hint : {word}")
 	user_input_letter = input("Guess a letter: ").lower()
 
 	# check answer
 	for letter in word:
 		if user_input_letter == letter:		
-			word_to_fill.append(user_input_letter)
-			print(word_to_fill)
+			placeholder += letter
 			break
-		elif lives == 0:
-			game = 0
+	for letter in word:
+		if user_input_letter != letter:
+			placeholder += '_'
 			break
-		else:
-			print(f"you guess {user_input_letter}. That's not in the word. You lose a life.")
-			print(hangman_art.stages[lives - 1])
-			print(f"************ You have {lives - 1}/6 lives left ************************")
-			lives -= 1
-			break
+	
+	print(f"{''.join(placeholder)}")			
+	
+	if lives == 0:
+		print(f"************ It was {guess_word}. Try again. ************************")
+		game = 0
+		break
+	if user_input_letter != letter:
+		print(f"you guess {user_input_letter}. That's not in the word. You lose a life")
+		print(hangman_art.stages[lives - 1])
+		print(f"************ You have {lives - 1}/6 lives left ************************")
+		lives -= 1
+	
