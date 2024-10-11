bee77c1d4b202038e034babd79f9bb0158d934bf
aesha98 <sityasya@gmail.com>
2024-10-11 10:19:31 +0800

    final version hangman game

--- Day_6_7/main.py
+++ Day_6_7/main.py
@@ -8,43 +8,48 @@ import string
 game = 1
 guess_word = random.choice(hangman_words.list_words)
 word_to_guess = guess_word.split()
-word = []
-word_to_fill = []
+placeholder = []
 lives = 6
-placeholder = ""
 
 # display logo
 print(hangman_art.logo)
 
-for char in len(guess_word):
+# blank guess
+for _ in range(len(guess_word)):
 	placeholder += '_'
-	word.append(char)
 
 # game running
 while(game):
-	print("word to guess: " + placeholder)
-	print(f"hint : {word}")
+
+	print(f"word to guess: {' '.join(placeholder)}")
 	user_input_letter = input("Guess a letter: ").lower()
 
+	if user_input_letter in placeholder:
+		print("You've already guess this")
+		
 	# check answer
-	for letter in word:
-		if user_input_letter == letter:		
-			placeholder += letter
-			break
-	for letter in word:
-		if user_input_letter != letter:
-			placeholder += '_'
-			break
+	for index in range(len(guess_word)):
+		letter = guess_word[index]
+
+		if user_input_letter == letter:
+			placeholder[index] = letter
 	
-	print(f"{''.join(placeholder)}")			
-	
-	if lives == 0:
-		print(f"************ It was {guess_word}. Try again. ************************")
-		game = 0
-		break
-	if user_input_letter != letter:
+	#check if user is wrong
+	if user_input_letter not in guess_word:
 		print(f"you guess {user_input_letter}. That's not in the word. You lose a life")
 		print(hangman_art.stages[lives - 1])
 		print(f"************ You have {lives - 1}/6 lives left ************************")
 		lives -= 1
+	
+		if lives == 0:
+			print(f"************ It was {guess_word}. Try again. ************************")
+			game = 0
+	
+	# check if user win
+	if '_' not in placeholder:
+		game = 0
+		print("You win")
+
+	#print(f"{' '.join(placeholder)}")			
+	
 	
