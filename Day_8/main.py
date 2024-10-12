74f592e9bff6bed9f2e2f0162688cf5fc2b3e8ca
aesha98 <sityasya@gmail.com>
2024-10-13 00:24:49 +0800

    update code caeser cipher

--- Day_8/main.py
+++ Day_8/main.py
@@ -2,15 +2,22 @@
 # caesar cipher program
 import logo
 
-def encode(message):
+def encode(message, shift_num):
 	print("encode")
 
-def decode(message):
+def decode(message, shift_num):
 	print("decode")
 
 print(logo.logo)
 
-encode_input = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-message = input("Type your message:\n")
-shift_number = input("Type the shift number:\n")
+typeInput = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 
+# check type of input encryption
+if (typeInput == 'encode'):
+	message = input("Type your message:\n")
+	shift_number = input("Type the shift number:\n")
+	encode(message, shift_number)
+elif (typeInput == 'decode'):
+	message = input("Type your message:\n")
+	shift_number = input("Type the shift number:\n")
+	decode(message, shift_number)
