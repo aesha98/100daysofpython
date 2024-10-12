ef43e11ff84dc6891c7740e98985582a39362437
aesha98 <sityasya@gmail.com>
2024-10-13 01:19:25 +0800

    update code

--- Day_8/main.py
+++ Day_8/main.py
@@ -5,7 +5,9 @@ import logo
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 def caeser(message, shift, type):
-	print("")
+	final_text = ""
+	
+	print(f"Here's the {type}d result : {final_text}")
 
 print(logo.logo)
 
