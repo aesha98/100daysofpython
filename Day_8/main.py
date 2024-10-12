7b29b68f4d345131ec4f2f1aeacdd74c9a547b1b
aesha98 <sityasya@gmail.com>
2024-10-13 00:11:36 +0800

    update day 8

---
+++ Day_8/main.py
@@ -0,0 +1,16 @@
+# author @aesha98
+# caesar cipher program
+import logo
+
+def encode(message):
+	print("encode")
+
+def decode(message):
+	print("decode")
+
+print(logo.logo)
+
+encode_input = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+message = input("Type your message:\n")
+shift_number = input("Type the shift number:\n")
+
